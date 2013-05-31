7237abd5595d3a9752d2bb0834db72f4b2eb9d16
Henrik N <henrik@nyh.se>
2013-05-31 14:41:15 +0200

    Conversion spec cleanup.

--- unit/entity/convert_spec.rb
+++ unit/entity/convert_spec.rb
@@ -3,6 +3,10 @@ require 'active_support/core_ext'
 
 describe Minimapper::Entity::Convert do
   describe ":integer" do
+    it "allows integers" do
+      described_class.new(10).to(:integer).should == 10
+    end
+
     it "converts strings into integers" do
       described_class.new('10').to(:integer).should == 10
       described_class.new(' 10 ').to(:integer).should == 10
@@ -10,13 +14,15 @@ describe Minimapper::Entity::Convert do
 
     it "makes it nil when it can't convert" do
       described_class.new(' ').to(:integer).should be_nil
-      described_class.new(' ').to(:date_time).should be_nil
       described_class.new('garbage').to(:integer).should be_nil
-      described_class.new('garbage').to(:date_time).should be_nil
     end
   end
 
   describe ":date_time" do
+    it "allows DateTimes" do
+      described_class.new(DateTime.new(2013, 6, 1)).to(:date_time).should == DateTime.new(2013, 6, 1)
+    end
+
     it "converts datetime strings into datetimes" do
       described_class.new('2012-01-01 20:57').to(:date_time).should == DateTime.new(2012, 01, 01, 20, 57)
     end
@@ -31,19 +37,11 @@ describe Minimapper::Entity::Convert do
     described_class.new('foobar').to(nil).should == 'foobar'
   end
 
-  it "does not make false nil" do
-    described_class.new(false).to(nil).should eq(false)
-  end
-
   it "raises when the type isn't known" do
     lambda { described_class.new('foobar').to(:unknown) }.should raise_error(/Unknown attribute type/)
   end
 
   it "does not make false nil" do
-    described_class.new(false).to(:unknown).should eq(false)
-  end
-
-  it "does not make false nil" do
-    described_class.new(false).to(:unknown).should eq(false)
+    described_class.new(false).to(:whatever).should eq(false)
   end
 end
